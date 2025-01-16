8b794c516c04521a17a868205696a35227b75632
AlexKomaha <107383667+AlexKomaha@users.noreply.github.com>
2025-01-16 10:27:17 +0900

    Added debagging

--- js/projects.js
+++ js/projects.js
@@ -3,3 +3,15 @@ const galleryButton = document.getElementById('galleryBtn');
 galleryButton.addEventListener('click', () => {
   window.location.href = 'gallery.html';
 });
+
+const galleryButton2 = document.getElementById('galleryBtn2');
+
+galleryButton2.addEventListener('click', () => {
+  window.location.href = 'gallery.html';
+});
+
+const galleryButton3 = document.getElementById('galleryBtn3');
+
+galleryButton3.addEventListener('click', () => {
+  window.location.href = 'gallery.html';
+});
